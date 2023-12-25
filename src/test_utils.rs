1b95193dc4ba62880f50f831abe635915be3c82f
Florian Plattner <florian@lasernoises.com>
2023-12-25 16:16:29 +0100

    Add border to test_utils::run_element

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -6,6 +6,7 @@ pub struct ElementStats {
     pages: u32,
     breaks: Vec<u32>,
     document: PdfDocumentReference,
+    size: Option<ElementSize>,
 }
 
 impl ElementStats {
@@ -22,15 +23,23 @@ impl ElementStats {
         assert!(breaks.into_iter().eq(self.breaks.iter().copied()));
         self
     }
+
+    pub fn assert_size(&self, size: Option<ElementSize>) -> &Self {
+        assert_eq!(self.size, size);
+        self
+    }
 }
 
 pub fn run_element<E: Element>(width: f64, height: f64, element: E) -> ElementStats {
     let (doc, page, layer) = PdfDocument::new("test", Mm(height), Mm(height), "Layer 0");
     let mut page_idx = 0;
 
+    let full_size = (width + 12.5, height + 12.5);
+    let pos = (2.5, height + 10.);
+
     let mut pdf = Pdf {
         document: doc,
-        page_size: (width, height),
+        page_size: full_size,
     };
 
     let mut breaks = vec![];
@@ -39,7 +48,8 @@ pub fn run_element<E: Element>(width: f64, height: f64, element: E) -> ElementSt
         breaks.push(draw_rect);
 
         while page_idx <= draw_rect {
-            pdf.document.add_page(Mm(width), Mm(height), "Layer 0");
+            pdf.document
+                .add_page(Mm(full_size.0), Mm(full_size.1), "Layer 0");
             page_idx += 1;
         }
 
@@ -48,10 +58,7 @@ pub fn run_element<E: Element>(width: f64, height: f64, element: E) -> ElementSt
             .get_page(PdfPageIndex((draw_rect + 1) as usize))
             .get_layer(PdfLayerIndex(0));
 
-        Location {
-            layer,
-            pos: (0.0, 297.0),
-        }
+        Location { layer, pos }
     };
 
     let layer = pdf.document.get_page(page).get_layer(layer);
@@ -59,10 +66,7 @@ pub fn run_element<E: Element>(width: f64, height: f64, element: E) -> ElementSt
     let ctx = DrawCtx {
         pdf: &mut pdf,
         width: Some(width),
-        location: Location {
-            layer,
-            pos: (0., height),
-        },
+        location: Location { layer, pos },
 
         first_height: height,
 
@@ -73,11 +77,12 @@ pub fn run_element<E: Element>(width: f64, height: f64, element: E) -> ElementSt
         }),
     };
 
-    element.draw(ctx);
+    let size = element.draw(ctx);
 
     ElementStats {
         pages: page_idx + 1,
         breaks,
         document: pdf.document,
+        size,
     }
 }
