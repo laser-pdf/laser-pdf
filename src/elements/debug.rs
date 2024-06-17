a125933e6228cb7074079e59526844fafbcbe060
Florian Plattner <florian@lasernoises.com>
2024-06-17 12:27:10 +0200

    Add color option to debug element

--- src/elements/debug.rs
+++ src/elements/debug.rs
@@ -2,15 +2,18 @@ use printpdf::{utils::calculate_points_for_rect, Line, Rgb};
 
 use crate::*;
 
-pub struct Debug<'a, E: Element>(pub &'a E);
+pub struct Debug<'a, E: Element + ?Sized> {
+    pub element: &'a E,
+    pub color: u8,
+}
 
-impl<'a, E: Element> Element for Debug<'a, E> {
+impl<'a, E: Element + ?Sized> Element for Debug<'a, E> {
     fn first_location_usage(&self, ctx: FirstLocationUsageCtx) -> FirstLocationUsage {
-        self.0.first_location_usage(ctx)
+        self.element.first_location_usage(ctx)
     }
 
     fn measure(&self, ctx: MeasureCtx) -> ElementSize {
-        self.0.measure(ctx)
+        self.element.measure(ctx)
     }
 
     fn draw(&self, ctx: DrawCtx) -> ElementSize {
@@ -18,10 +21,12 @@ impl<'a, E: Element> Element for Debug<'a, E> {
         let first_location_height;
         let location = ctx.location.clone();
 
+        let color = calculate_color(self.color);
+
         if let Some(breakable) = ctx.breakable {
             let mut break_count = 0;
 
-            size = self.0.draw(DrawCtx {
+            size = self.element.draw(DrawCtx {
                 pdf: ctx.pdf,
                 breakable: Some(BreakableDraw {
                     do_break: &mut |pdf, location_idx, height| {
@@ -62,23 +67,41 @@ impl<'a, E: Element> Element for Debug<'a, E> {
                         }),
                     );
 
-                    draw_box(location, (width, height));
+                    draw_box(location, (width, height), color);
                 }
             }
         } else {
-            size = self.0.draw(ctx);
+            size = self.element.draw(ctx);
             first_location_height = size.height;
         }
 
         if let (Some(width), Some(height)) = (size.width, first_location_height) {
-            draw_box(location, (width, height));
+            draw_box(location, (width, height), color);
         }
 
         size
     }
 }
 
-fn draw_box(location: Location, size: (f64, f64)) {
+fn hue_to_rgb(hue: u8) -> [u8; 3] {
+    let x = 6u8.saturating_mul(43 - 43u8.abs_diff(hue % 85));
+
+    match hue / 43 {
+        0 => [255, x, 0],
+        1 => [x, 255, 0],
+        2 => [0, 255, x],
+        3 => [0, x, 255],
+        4 => [x, 0, 255],
+        5 => [255, 0, x],
+        _ => unreachable!(),
+    }
+}
+
+fn calculate_color(input: u8) -> [f64; 3] {
+    hue_to_rgb(input.reverse_bits()).map(|c| c as f64 / 255.)
+}
+
+fn draw_box(location: Location, size: (f64, f64), color: [f64; 3]) {
     let points = calculate_points_for_rect(
         Mm(size.0),
         Mm(size.1),
@@ -92,7 +115,9 @@ fn draw_box(location: Location, size: (f64, f64)) {
 
     location
         .layer
-        .set_outline_color(printpdf::Color::Rgb(Rgb::new(0., 1., 0., None)));
+        .set_outline_color(printpdf::Color::Rgb(Rgb::new(
+            color[0], color[1], color[2], None,
+        )));
 
     location.layer.add_shape(Line {
         points,
